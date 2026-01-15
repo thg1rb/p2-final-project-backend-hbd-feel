66c1a0e04d213c698158a89823bb88749e2ab072
Rugsit <rugsit.nest@gmail.com>
2026-01-15 19:56:34 +0700

    feat(dashboard): add MainDashboardController and routes

--- app/Http/Controllers/MainDashboardController.php
+++ app/Http/Controllers/MainDashboardController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MainDashboardController extends Controller
 {
-    //
+    public  function index()
+    {
+        $data = [
+            'all-user' => 20,
+            'round' => '2569/1',
+            'request' => 100,
+        ];
+        return view("main.dashboard", [
+            'data' => $data
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\MainDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('main.dashboard');
-})->name('main.dashboard');
+Route::get('/', [MainDashboardController::class, 'index'])->name('main.dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
