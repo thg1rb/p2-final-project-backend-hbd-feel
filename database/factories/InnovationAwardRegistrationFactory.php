e095b0c70bbf516168468a698dec4b0ceef3c263
TeeAnusonsart <anusonsart@gmail.com>
2026-01-24 22:09:07 +0700

    create registration factories and seeder

---
+++ database/factories/InnovationAwardRegistrationFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InnovationAwardRegistration>
+ */
+class InnovationAwardRegistrationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'award_name' => $this->faker->firstName(),
+        ];
+    }
+}
