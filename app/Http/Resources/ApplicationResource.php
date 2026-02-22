941c882b76645ca3108099d5ca48a9063ca7f0e8
nk-n <narakorn458@gmail.com>
2026-02-22 22:24:24 +0700

    feat(my-awards): move AwardRegistrationController logic to ApplicationController and improve response structure

---
+++ app/Http/Resources/ApplicationResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ApplicationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'application_id' => $this->resource->id,
+            'academic_year' => $this->resource->event->academic_year,
+            'semester' => $this->resource->event->semester,
+            'status' => $this->resource->status,
+            'path' => $this->resource->path,
+            'level' => $this->resource->level,
+            'grade' => $this->resource->grade,
+            'document' => $this->resource->document,
+            'year' => $this->resource->year,
+            'created_at' => $this->resource->created_at,
+            'updated_at' => $this->resource->updated_at,
+            'award_name' => $this->resource->award->name,
+            'form_path' => $this->resource->award->form_path,
+            'requirements' => $this->resource->requirements,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,18 @@
 
 use App\Http\Controllers\Api\ApplicationController;
 use App\Http\Controllers\Api\ApprovalController;
-use App\Http\Controllers\Api\AwardRegistrationController;
 use App\Http\Controllers\Api\MinioController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/approvals', [ApprovalController::class, 'store']);
-
-Route::resource('/award-registrations', AwardRegistrationController::class);
 
 Route::get('/applications', [ApplicationController::class, 'getAllApplications']);
 
 Route::get('/applications/count', [ApplicationController::class, 'getApplicationCountByStatus']);
 
 Route::get('/application/{id}', [ApplicationController::class, 'getApplicationById']);
+
+Route::get('/application/student/{id}', [ApplicationController::class, 'getApplicationByStudentId']);
 
 Route::get('/minio/download', [MinioController::class, 'getPreviewUrl']);
 
